7fbca792611289ff21f058291e6459fc5fdb9b15
Adriel Dube <adrieldubemarante@gmail.com>
2026-03-09 22:34:09 -0600

    Add new styles for flight and frame components; update build process to include minified CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     content: [
         "./index.html",
         "./es/index.html",
+        "./flight.html",
+        "./es/flight.html",
         "./frame.html",
+        "./es/frame.html",
         "./js/**/*.js"
     ],
     theme: {
